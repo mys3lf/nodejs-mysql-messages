9b2f96c53e264e98fcf90fce125cd9d3f7ba0f8b
Matthäus <mys3lf@recalot.com>
2017-09-06 13:32:15 +0200

    Initial commit

---
+++ messages.js
@@ -0,0 +1,89 @@
+var express = require('express'), mysql = require('mysql'), bodyParser = require('body-parser');
+var app = express();
+
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: 'dummy',
+    database: 'messages'
+});
+
+var server = app
+
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({
+        extended: true
+    }))
+    .listen(8081, function () {
+        var host = server.address().address
+        var port = server.address().port
+
+        console.log("Server listing at http://%s:%s", host, port)
+    });
+
+
+app.get('/messages', function (req, res) {
+    pool.query('SELECT id, type, host, title, timestamp, content from Messages', function (error, results, fields) {
+        if (error) throw error;
+
+        var array = [];
+
+        for (var i = 0; i < results.length; i++) {
+            array.push(
+                {
+                    id: results[i].id,
+                    type: results[i].type,
+                    host: results[i].host,
+                    title: results[i].title,
+                    timestamp: results[i].timestamp,
+                    content: results[i].content
+                }
+            );
+        }
+
+        res.json(array);
+    });
+}).get("/messages/:messageId", function (req, res) {
+    pool.query('SELECT id, type, host, title, timestamp, content from Messages where id= ?', [req.params.messageId], function (error, results, fields) {
+        if (error) throw error;
+        var array = [];
+
+        if (results.length > 0) {
+            res.json({
+                id: results[0].id,
+                type: results[0].type,
+                host: results[0].host,
+                title: results[0].title,
+                timestamp: results[0].timestamp,
+                content: results[0].content
+            });
+        } else {
+            res.json({
+                "message": "not found"
+            });
+        }
+    });
+}).post("/messages", function (req, res) {
+    if (req.body.type != null && req.body.content != null && req.body.host != null && req.body.title) {
+        pool.query("INSERT INTO Messages (type, host, title, content) VALUES (?, ?, ?, ?)", [req.body.type, req.body.host, req.body.title, req.body.content], function (error, results, fields) {
+
+            if (error != null) {
+                res.json({
+                    "message": "error"
+                });
+            } else {
+                res.json({
+                    "message": "success"
+                });
+            }
+
+        });
+    } else {
+        res.status(400);
+        res.json({
+            "message": "missing attributes"
+        });
+    }
+
+})
